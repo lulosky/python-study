1de6bc2f0355d4485ffceb1bb77593cd8b065d8a
Luba <FamSanchez@MacBook-Pro-de-Luba.local>
2018-10-21 21:53:42 -0600

    Reading for the file

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-def felixon(word):
-    response = '{} I see and I like Meow sometimes'.format(word)
-    return response
+text = open('boshka.txt', 'r')
 
-def FoodOfCat(word):
-    catFood = '{} eat tuna and bones'.format(word)
-    return catFood
+formatedText = text.read()
 
-
-def boshka(word, callback):
-    groupWords = "{} I'm Boshka and I like to".format(word)
-    amountOfLetters = str(len(groupWords))
-    response = felixon(groupWords)
-    catFood = FoodOfCat(groupWords)
-    callback(groupWords, amountOfLetters, response, catFood)
-
-
-
-boshka('hello', lambda a, b, c, d: print('{} Meow'.format(a), '**The amount of letters is {}**'.format(b), 'Response from Felixon: {}'.format(c), 'Answer from BoshkaNieves {}'.format(d)))
+print( 'This is a text', formatedText)
+text.close()
