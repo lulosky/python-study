e7a4db311df78b550eca97af461277d45e8583df
Luba <FamSanchez@MacBook-Pro-de-Luba.local>
2018-11-06 09:02:17 -0600

    camelot read internal file

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-with open("boshka/nieves.txt", "r") as felix:
-    cat = felix.read()
-    print(cat)
+# with open('boshka.txt', 'a') as f:
+#     boshka = f.write('My boshka beautiful and fun')
+# with open('boshka.txt', 'r') as b:
+#     boshka = b.read()
+#     print(boshka)
+
+with open('camelot.txt', 'r') as t:
+    camelot0 = t.read(2)
+    camelot1 = t.read(8)
+    camelot2 = t.read()
+    print(camelot0, camelot1, camelot2)
