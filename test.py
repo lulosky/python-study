2ef2d64cf94637313d4ca499a5e07b96a7072f79
Luba <FamSanchez@MacBook-Pro-de-Luba.local>
2018-11-14 17:57:49 -0600

    callback function example

--- test.py
+++ test.py
@@ -4,8 +4,15 @@
 #     boshka = b.read()
 #     print(boshka)
 
-with open('camelot.txt', 'r') as t:
-    camelot0 = t.read(2)
-    camelot1 = t.read(8)
-    camelot2 = t.read()
-    print(camelot0, camelot1, camelot2)
+# with open('camelot.txt', 'r') as t:
+#     camelot0 = t.read(3)
+#     camelot1 = t.read(8)
+#     camelot2 = t.read()
+#     print(camelot0)
+
+def boshka(number, boshkaCallback):
+    amountMeows = "{} times".format(number)
+    boshkaCallback(amountMeows)
+
+
+boshka(4, lambda a: print("Meow {}".format(a)))
