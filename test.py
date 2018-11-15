f500a5a1565d5e967befee3d21357560fa50e51d
Luba <FamSanchez@MacBook-Pro-de-Luba.local>
2018-11-14 19:28:34 -0600

    flying-circus-cast-quiz

--- test.py
+++ test.py
@@ -1,18 +1,7 @@
-# with open('boshka.txt', 'a') as f:
-#     boshka = f.write('My boshka beautiful and fun')
-# with open('boshka.txt', 'r') as b:
-#     boshka = b.read()
-#     print(boshka)
-
-# with open('camelot.txt', 'r') as t:
-#     camelot0 = t.read(3)
-#     camelot1 = t.read(8)
-#     camelot2 = t.read()
-#     print(camelot0)
-
-def boshka(number, boshkaCallback):
-    amountMeows = "{} times".format(number)
-    boshkaCallback(amountMeows)
-
-
-boshka(4, lambda a: print("Meow {}".format(a)))
+def create_cast_list(filename):
+    cast_list = []
+    with open(filename, "r") as t:
+        for i in t:
+            list = i.split(',')
+            cast_list.append(list[0])
+    return(cast_list)
