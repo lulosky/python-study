7b70f24c55b41fce3dd45de82d1a4904a8af8fe4
Luba <FamSanchez@MacBook-Pro-de-Luba.local>
2018-11-25 11:21:20 -0600

    HOMEWORK1

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-def create_cast_list(filename):
-    cast_list = []
-    with open(filename, "r") as t:
-        for i in t:
-            list = i.split(',')
-            cast_list.append(list[0])
-    return(cast_list)
+# Create a function with 1 argument that determine if a number is able to devide by 3 and 5
+# a number able to devide by 3 and 5 have to return Boshka Nieves
+# a number only able to devide by 3 return Boshka
+# a number only able to devide by 5 return Nieves
+# print the result to the terminal
+
+
+def homework(argument):
+
+
+
+    if argument % 3 == 0 and argument % 5 == 0:
+        return "Boshka Nieves"
+    elif argument % 3 == 0:
+        return "Boshka"
+
+    elif argument % 5 == 0:
+        return "Nieves"
+
+
+print(homework(5))
